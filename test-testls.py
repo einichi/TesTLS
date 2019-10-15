fccb7e4c76ce312c6e069ba7dd6e4813745174c7
einichi <ricky@burg.in>
2019-10-15 09:14:27 +0000

    systemd in docker is bad. testing now excludes systemd dependencies

--- test-testls.py
+++ test-testls.py
@@ -2,7 +2,7 @@
 # Test your TesTLS before you test your TLS! :s
 
 scriptname = 'testls.sh'
-outputfile = 'no-le-testls.sh'
+outputfile = 'no-le-or-systemd-testls.sh'
 
 with open(outputfile, 'w') as output:
     blockcomment = False
@@ -25,6 +25,10 @@ with open(outputfile, 'w') as output:
         elif "CIPHERS=" in line:
             output.write("CIPHERS=\"TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ARIA256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA\"\n")
         elif "/usr/local/bin/certbot-auto certonly" in line:
+            output.write("# " + line)
+        elif "firewall-cmd" in line:
+            output.write("# " + line)
+        elif "systemctl" in line:
             output.write("# " + line)
         elif "reboot" in line:
             output.write("# " + line)
